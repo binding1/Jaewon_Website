2698446d53b716f0ef5707ea952cee9d87e7d970
Jaewon Bae <jaewon@Jaewons-MacBook-Pro.local>
2022-12-29 23:59:59 -0500

    add express, nodemon, ejs

---
+++ server.js
@@ -0,0 +1,16 @@
+const express  = require("express"); // Import the express library
+const app = express(); // Define our app as an instance of express
+const port = 3000; // Define our base URL as http:\\localhost:3000
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// index page
+app.get('/', function(req, res) {
+    res.render('index');
+  });
+
+
+app.listen(port, function () {
+  console.log(`Server running on port ${port}`); // Tell yourself the port number to prevent mistakes in the future.
+});
